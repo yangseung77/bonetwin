4f5a1b68eb9ac92b4bccddd67c7f886f149895c0
mis2n <57153699+mis2n@users.noreply.github.com>
2023-10-05 10:08:01 -0500

    adding help screen component

---
+++ src/app/help-screen/help-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-screen',
+  templateUrl: './help-screen.component.html',
+  styleUrls: ['./help-screen.component.scss']
+})
+
+export class HelpScreenComponent {
+
+}
