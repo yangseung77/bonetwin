a6697bf638534b971e242571976f6287d8f212d0
Marcus615 <marcusstephens808@gmail.com>
2023-10-05 14:30:43 -0500

    routing is bugged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoneCalcComponent } from './bone-calc/bone-calc.component';
+import { HelpScreenComponent } from './help-screen/help-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BoneCalcComponent,
+    
+  },
+
+  {
+    path: 'home',
+    component: BoneCalcComponent,
+    
+  },
+
+  {
+    path: 'help-screen',
+    component: HelpScreenComponent,
+   
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
