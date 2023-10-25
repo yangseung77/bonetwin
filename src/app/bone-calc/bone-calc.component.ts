57fc169ef6584321cfaf0d9c46edd45b13ab8a6d
SamualKingMTSU <124930940+SamualKingMTSU@users.noreply.github.com>
2023-10-25 10:29:13 -0500

    Updated the build after previous change.

--- src/app/bone-calc/bone-calc.component.ts
+++ src/app/bone-calc/bone-calc.component.ts
@@ -218,7 +218,7 @@ getStatValueMono(statArr: any, gender: any, bone: any, choice: any, prob: number
     decimal = Number(prob.toFixed(7))
     //console.log(statArr[i].boneStats)
     closest = this.closestIndexMono(decimal, tempArr);
-      data = "Sensitivity: " + tempArr[closest].sensitivity + "\n" + "Specificity " + tempArr[closest].specificity;
+      data = "Sensitivity: " + tempArr[closest].sensitivity + "\n" + "1-Specificity: " + tempArr[closest].specificity;
 
     //}
   
